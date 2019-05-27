7c475b17222deb36625752062dedd7f5c8f236e2
rhn014 <Roland.Hyden@RegionHalland.se>
2019-05-27 15:24:01 +0200

    justerat versionsnummer

--- region_halland_acf_drift_info.php
+++ region_halland_acf_drift_info.php
@@ -6,7 +6,7 @@
     /*
     Plugin Name: Region Halland ACF Drift Info
     Description: ACF-fält för drift info
-    Version: 2.0.0
+    Version: 2.0.1
     Author: Roland Hydén
     License: Free to use
     Text Domain: regionhalland
